d8a09f563301c6a37e1e02ecbeb4d6f4dcbe574d
sean <y20120816s@gmail.com>
2026-05-03 15:15:20 +0800

    DEBUG-5

--- api/login.php
+++ api/login.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-$acc = $_POST['acc']; 
-$pw = $_POST['pw'];
-// $num_ver = $_POST['num_ver'];
-if($acc == 'admin' && $pw == '1234'){
-    $_SESSION['users']='admin';
+
+$acc = $_POST['acc'] ?? '';
+$pw  = $_POST['pw']  ?? '';
+
+if ($acc === 'admin' && $pw === '1234') {
+    $_SESSION['users'] = 'admin';
     echo 1;
-}else{
+} else {
     echo 0;
 }
