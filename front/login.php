9486020bc4ae25f2cf7a9b6097db8494d88f8542
sean <y20120816s@gmail.com>
2026-05-03 15:22:17 +0800

    登入Bug解決

--- front/login.php
+++ front/login.php
@@ -1,15 +1,15 @@
 <h2 class="text-center">網站管理</h2>
-<form action="./api/login.php" method="post">
+<form id="LoginForm">
     <div class="form-group">
         <label for="acc">帳號</label>
         <input type="text" class="form-control" name="acc">
     </div>
     <div class="form-group">
-        <label for="">密碼</label>
-        <input type="text" class="form-control" name="pw">
+        <label for="pw">密碼</label>
+        <input type="password" class="form-control" name="pw">
     </div>
     <div class="form-group">
-        <label for="">驗證碼</label>
+        <label for="num_ver">驗證碼</label>
         <input type="text" class="form-control" name="num_ver">
         <input type="text" class="btn btn-warning" value="4567">
         <input type="text" class="btn btn-secondary" value="重新產生驗證碼">
