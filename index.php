41fe1a1eb834d13393c1ca850948bb3670c92143
sean <y20120816s@gmail.com>
2026-05-03 09:56:10 +0800

    scr jquery

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo phpinfo() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>56選手練習培訓(全國賽)</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+</head>
+<body>
+    <h1>56選手練習培訓(全國賽)</h1>
+    
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/vue.3.5.13.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script>
+        $("body").html("HI")  // JQUERY 測試語句
+    </script>
+</body>
+</html>
