0a2a70817bdc45f2fba6d8d8955f1da002b90502
sean <y20120816s@gmail.com>
2026-05-03 14:44:21 +0800

    Bug修復所需檔案(3)

---
+++ modals/add_route.php
@@ -0,0 +1,28 @@
+<div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">新增路線</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post">
+            <div>
+                <label for="">路線名稱</label>
+                <input type="text">
+            </div>
+            <div>
+                <label for="">每列站點數</label>
+                <input type="number">
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
