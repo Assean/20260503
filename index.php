d8d92e1e2800a028987315aa69ea1af099ed2f22
sean <y20120816s@gmail.com>
2026-05-03 11:07:02 +0800

    2

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
     <main>
         <div class="form-group mt-3">
             <select name="route" id="" class="d-block m-auto col-6 form-control">
-                <option class="" value="1">路線1</option>
-                <option class="" value="2">路線2</option>
-                <option class="" value="3">路線3</option>
-                <option class="" value="4">路線4</option>
-                <option class="" value="5">路線5</option>
+                <option value="1">路線1</option>
+                <option value="2">路線2</option>
+                <option value="3">路線3</option>
+                <option value="4">路線4</option>
+                <option value="5">路線5</option>
             </select>
         </div>
         <div class="w-full" id="RouteMap">路網圖</div>
